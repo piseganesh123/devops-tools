351f56d8d15154230a80fa6a2c0f659c2a7b4679
piseganesh123 <piseganesh123@gmail.com>
2023-09-22 09:41:40 +0000

    updated fluentbit conf file

--- logging-fluentbit/output.tf
+++ logging-fluentbit/output.tf
@@ -1,3 +1,7 @@
-output "server_public_ip" {
+output "k8s server_public_ip" {
   value = google_compute_instance.k8s_master_server.network_interface.0.access_config.0.nat_ip
+}
+
+output "logging server_public_ip" {
+  value = google_compute_instance.logging_master_server.network_interface.0.access_config.0.nat_ip
 }
