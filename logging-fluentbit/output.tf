f7244a0a52aeee070b0ffdc1b038c7947d77ae26
piseganesh123 <piseganesh123@gmail.com>
2023-09-25 10:15:41 +0000

    updated monitoring tooling code

--- logging-fluentbit/output.tf
+++ logging-fluentbit/output.tf
@@ -1,7 +1,7 @@
-output "k8s server_public_ip" {
+output "k8s_server_public_ip" {
   value = google_compute_instance.k8s_master_server.network_interface.0.access_config.0.nat_ip
 }
 
-output "logging server_public_ip" {
+output "logging_server_public_ip" {
   value = google_compute_instance.logging_master_server.network_interface.0.access_config.0.nat_ip
 }
